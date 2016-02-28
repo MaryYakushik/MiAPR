360a7276fd963fa5fc385d8dc969d92d08f2c29a
MaryYakushik <mary.yakushik@gmail.com>
2016-02-28 17:25:28 +0300

    changes

--- Laba2/Laba2/Form1.Designer.cs
+++ Laba2/Laba2/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.pointsCountBox = new System.Windows.Forms.TextBox();
             this.acceptButton = new System.Windows.Forms.Button();
             this.findClassesButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -72,16 +73,29 @@
             this.findClassesButton.UseVisualStyleBackColor = true;
             this.findClassesButton.Click += new System.EventHandler(this.findClassesButton_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(740, 398);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.findClassesButton);
             this.Controls.Add(this.acceptButton);
             this.Controls.Add(this.pointsCountBox);
             this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Laba 2";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -95,6 +109,7 @@
         private System.Windows.Forms.TextBox pointsCountBox;
         private System.Windows.Forms.Button acceptButton;
         private System.Windows.Forms.Button findClassesButton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Laba2/Laba2/Form1.cs
+++ Laba2/Laba2/Form1.cs
@@ -40,31 +40,49 @@ namespace Laba2
 
         private void acceptButton_Click(object sender, EventArgs e)
         {
+            bufferedGraphics.Graphics.Clear(pictureBox1.BackColor);
+            bufferedGraphics.Render();
             points = new List<Point>();
             try
             {
                 int countOfPoints = int.Parse(pointsCountBox.Text);
-                for (int i = 0; i < countOfPoints; i++)
+                if (countOfPoints >= 1000 && countOfPoints <= 100000)
                 {
-                    Point p = new Point();
-                    p.x = r.Next(pictureBox1.Size.Width);
-                    p.y = r.Next(pictureBox1.Size.Height);
-                    points.Add(p);
-                    bufferedGraphics.Graphics.FillRectangle(new SolidBrush(Color.White),
-                       p.x, p.y, 2, 2);
+                    for (int i = 0; i < countOfPoints; i++)
+                    {
+                        Point p = new Point();
+                        p.x = r.Next(pictureBox1.Size.Width);
+                        p.y = r.Next(pictureBox1.Size.Height);
+                        points.Add(p);
+                        bufferedGraphics.Graphics.FillRectangle(new SolidBrush(Color.Black),
+                           p.x, p.y, 2, 2);
+                    }
+                    bufferedGraphics.Render();
                 }
-                bufferedGraphics.Render();
+                else
+                {
+                    MessageBox.Show("Введите значение в диапазоне от 1000 до 100000");
+                }
             }
-            catch { }
+            catch 
+            {
+                MessageBox.Show("Введите значение в диапазоне от 1000 до 100000");
+            }
         }
 
+        bool t;
         private void findClassesButton_Click(object sender, EventArgs e)
         {
             numbersOfCenters = new List<int>();
             centers = new List<Point>();
             setRandomCenter();
             findMaxDistance(0);
-            spreadPoints();
+            t = true;
+            while (t)
+            {
+                spreadPoints();
+                findmaxDistanceInClasses();
+            }
             repaint();
             
         }
@@ -110,6 +128,7 @@ namespace Laba2
             p.y = points[numberOfPoint].y;
             p.numberOfCenter = numberOfPoint;
             centers.Add(p);
+            numbersOfCenters.Add(numberOfPoint);
         }
 
         private void spreadPoints()
@@ -153,5 +172,76 @@ namespace Laba2
             bufferedGraphics.Render();
         }
 
+        SortedList<Double, int> maxDistances;
+        Double averageDistance;
+
+        private void findmaxDistanceInClasses()
+        {
+            averageDistance = 0;
+            maxDistances = new SortedList<Double, int>();           
+            Parallel.For(0, centers.Count, functionForThreads);
+            Parallel.For(0, centers.Count, Average);
+            Double element = maxDistances.ElementAt(maxDistances.Count - 1).Key;
+            if(element > averageDistance / (2 * centers.Count))
+            {
+                int numberOfElement = maxDistances.ElementAt(maxDistances.Count - 1).Value;
+                Point p = new Point();
+                p.x = points[numberOfElement].x;
+                p.y = points[numberOfElement].y;
+                p.numberOfCenter = numberOfElement;
+                centers.Add(p);
+                numbersOfCenters.Add(numberOfElement);
+            }
+            else
+            {
+                t = false;
+            }
+        }
+
+        private void functionForThreads( int i)
+        {
+            double maxdistance = -1;
+            int numberOfMaxDistance = -1;
+            for (int j = 0; j < points.Count; j++)
+            {
+                if (!numbersOfCenters.Contains(j))
+                {
+                    if (points[j].numberOfCenter == i)
+                    {
+                        if (maxdistance < 0)
+                        {
+                            maxdistance = Math.Pow(centers[i].x - points[j].x, 2) + Math.Pow(centers[i].y - points[j].y, 2);
+                            numberOfMaxDistance = j;
+                        }
+                        else
+                        {
+                            double distance = Math.Pow(centers[i].x - points[j].x, 2) + Math.Pow(centers[i].y - points[j].y, 2);
+                            if (distance > maxdistance)
+                            {
+                                maxdistance = distance;
+                                numberOfMaxDistance = j;
+                            }
+                        }
+                    }
+                }
+                
+            }
+            maxDistances.Add(maxdistance, numberOfMaxDistance);
+        }
+
+        private void Average(int numberOfCurrentCenter)
+        {
+            for (int i = numberOfCurrentCenter + 1; i < centers.Count; i++)
+            {
+                double distance = Math.Pow(centers[numberOfCurrentCenter].x - centers[i].x, 2) + Math.Pow(centers[numberOfCurrentCenter].y - centers[i].y, 2);
+                averageDistance += distance;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bufferedGraphics.Graphics.Clear(pictureBox1.BackColor);
+            bufferedGraphics.Render();
+        }
     }
 }
