8460a4936bcd0220ba6061a757b91a6872ef94a6
MaryYakushik <mary.yakushik@gmail.com>
2016-03-01 00:21:44 +0300

    Laba3

---
+++ Laba3/Laba3/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Laba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0,3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(236, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "0,7";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(364, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(719, 321);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "P(C1)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "P(C2)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 371);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Laba 3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Laba3/Laba3/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = pictureBox1.CreateGraphics();
+            bufferedGraphicsContext = new BufferedGraphicsContext();
+            bufferedGraphics = bufferedGraphicsContext.Allocate(graphics,
+                new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+
+            bufferedGraphics.Graphics.Clear(pictureBox1.BackColor);
+        }
+
+
+        Graphics graphics;
+        BufferedGraphicsContext bufferedGraphicsContext;
+        BufferedGraphics bufferedGraphics;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bufferedGraphics.Graphics.Clear(pictureBox1.BackColor);
+                bufferedGraphics.Render();
+                int pointsCount = 1000;
+                double PC1 = double.Parse(textBox1.Text);
+                double PC2 = double.Parse(textBox2.Text);
+
+                List<int> points1 = new List<int>();
+                List<int> points2 = new List<int>();
+
+                if(PC1 + PC2 - 1 < 0.00000000001)
+                {
+                    Random r = new Random();
+                    double mu1 = 0;
+                    double mu2 = 0;
+                    for (int i = 0; i < pointsCount; i++)
+                    {
+                        points1.Add(r.Next(pictureBox1.Width) - 100);
+                        points2.Add(r.Next(pictureBox1.Width) + 100);
+                        mu1 += points1[i];
+                        mu2 += points2[i]; 
+                    }
+                    mu1 /= pointsCount;
+                    mu2 /= pointsCount;
+
+                    double sigma1 = 0;
+                    double sigma2 = 0;
+                    for (int i = 0; i < pointsCount; i++)
+                    {
+                        sigma1 += Math.Pow(points1[i] - mu1, 2);
+                        sigma2 += Math.Pow(points2[i] - mu2, 2);
+                    }
+                    sigma1 = Math.Sqrt(sigma1 / pointsCount);
+                    sigma2 = Math.Sqrt(sigma2 / pointsCount);
+
+                    double p1 = 0;
+                    double p2 = 0;
+                    Color c1 = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+                    Color c2 = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+                    for(int i = 0; i < pointsCount; i++)
+                    {
+                        p1 = Math.Exp(-0.5 * Math.Pow((i - mu1) / sigma1, 2)) /
+                            (sigma1 * Math.Sqrt(2 * Math.PI));
+                        p2 = Math.Exp(-0.5 * Math.Pow((i - mu2) / sigma2, 2)) /
+                             (sigma2 * Math.Sqrt(2 * Math.PI));
+                        bufferedGraphics.Graphics.FillRectangle(new SolidBrush(c1),
+                             i, pictureBox1.Height - (int) (p1 * PC1 * 150000), 3, 3);
+                        bufferedGraphics.Graphics.FillRectangle(new SolidBrush(c2),
+                             i, pictureBox1.Height - (int) (p2 * PC2 * 150000), 3, 3);
+                    }
+                    bufferedGraphics.Render();
+                }
+                else
+                {
+                    MessageBox.Show("Сумма вероятностей должна равняться 1");
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
